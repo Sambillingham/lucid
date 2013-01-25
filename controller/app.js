74e15aac85c3218220dc1675a5945d4e596b3387
sambillingham <sambillie.sb@gmail.com>
2013-01-25 03:06:55 +0000

    Added Express, some CSS & removed redundent code

--- controller/app.js
+++ controller/app.js
@@ -1,6 +1,7 @@
-var app = require('http').createServer(handler),
-    io = require('socket.io').listen(app),
-    fs = require('fs'),
+var express = require('express'),
+    app = express(),
+    server = require('http').createServer(app),
+    io = require('socket.io').listen(server),
     osc = require('node-osc');
 
 var XRoll = 0,
@@ -9,26 +10,33 @@ var XRoll = 0,
     player2 = 0,
     player1Taken = false,
     player2Taken = false,
+    lookZ = 0,
     lookX = 0,
-    lookY = 0,
-    lookDirection = 0;
+    lookY = 0;
 
 var client = new osc.Client('192.168.0.5', 3333);
 
-app.listen(8080);
 
-function handler (req, res) {
-  fs.readFile(__dirname + '/index.html',
-  function (err, data) {
-    if (err) {
-      res.writeHead(500);
-      return res.end('Error loading index.html');
-    }
 
-    res.writeHead(200);
-    res.end(data);
-  });
-}
+server.listen(8080);
+
+app.configure(function() {
+
+  app.use(express.static(__dirname + '/public'));
+
+});
+
+app.get('/', function (req, res) {
+
+  switch(req.url) {
+    case '/':
+    case '/index.html':
+      res.sendfile(__dirname + '/index.html');
+    break
+    
+
+  }
+});
 
 
 io.sockets.on('connection', function (socket) {
@@ -36,73 +44,75 @@ io.sockets.on('connection', function (socket) {
     socket.emit('playerID', socket.id);
     
 
-    
+    //Sockets for Player Selection
 
     socket.on('iamplayer1', function(data) {
+
       player1 = data;
       console.log("Player1: " + player1 + ":     :" + "Player2: " + player2);
       socket.emit('youArePlayer1' );
 
     });
+
     socket.on('iamplayer2', function(data) {
+
       player2 = data;
       console.log("Player1: " + player1 + ":     :" + "Player2: " + player2);
       socket.emit('youArePlayer2' );
+
     });
+    // END Player Selection
+
+    // Sockets for Move Actions
 
     socket.on('moveRollX', function (data) {
-        if (data > 10 || data < -10){
+        
         XRoll = data;
         sendOSCMove();
-      } else {
-        XRoll = 0;
-        sendOSCMove();
-      }
-        
         
     });
 
     socket.on('moveRollY', function (data) {
-      if (data > 10 || data < -10){
-      YRoll = data;
+
+      if (data > 10 || data < -10) {
+
+        YRoll = data;
         sendOSCMove();
+
       } else {
+
         YRoll = 0;
         sendOSCMove();
+
       }
     });
+    // END Move actions
 
-    //Sockets for look actions
+    // Sockets for look actions
 
     socket.on('lookRollX', function (data) {
 
-        lookX = data;
-        sendOSCLook();
+      lookZ = data;
+      sendOSCLook();
 
         
     });
 
     socket.on('lookRollY', function (data) {
       
-      lookY = data;
+      lookX = data;
       sendOSCLook();
       
     });
 
     socket.on('lookDirection', function (data) {
       
-      lookDirection = data;
+      lookY = data;
       sendOSCLook();
       
     });
 
     // END look actions
-
-   // socket.on('direction', function (data) {
-       // console.log(' Device Direction : ', data);
-   // });
-
-
 
 });
 
@@ -111,8 +121,8 @@ function sendOSCMove(){
   console.log('X and Y Axis Data',  "     X :" + XRoll + "     Y :" + YRoll );
 }
 function sendOSCLook(){
-  client.send('/look', lookX, lookY, lookDirection);
-  console.log('Looking Data', "     X :" + lookX + "     Y :" + lookY + ":     D :" + lookDirection );
+  client.send('/look', lookZ, lookX, lookY);
+  console.log('Looking Data', "     Z :" + lookZ + "     X :" + lookX + ":     Y :" + lookY );
 }
 
 

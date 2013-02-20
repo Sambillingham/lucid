5a0c4a889caf778035eb67934573e987aced036d
bloomingbridges <brueckner.florian@googlemail.com>
2013-02-20 00:38:28 +0000

    Added challenge rooms with bad lighting.

--- unity/Assets/Scripts/GyroInputController.js
+++ unity/Assets/Scripts/GyroInputController.js
@@ -1,18 +1,20 @@
-private var motor : CharacterMotor;
+public var motor : CharacterController;
 public var sight : GameObject;
 private var sightTo : Quaternion;
 private var rotateBy : float = 0f;
 
 // Use this for initialization
 function Awake () {
-	motor = GetComponent(CharacterMotor);
+	motor = GetComponent(CharacterController);
 	sight = gameObject;
 	camRotation = sight.transform.rotation;
 }
 
 function Update () {
-	gameObject.transform.RotateAround(transform.position, new Vector3(0,1,0), Time.deltaTime * rotateBy);
-	// gameObject.transform.rotation = Quaternion.Slerp(gameObject.transform.rotation, Quaternion.Euler(gameObject.transform.rotation.x, gameObject.transform.rotation.y + rotateBy, gameObject.transform.rotation.z), Time.deltaTime * rotateBy);
+
+	//gameObject.transform.RotateAround(transform.position, new Vector3(0,1,0), Time.deltaTime * rotateBy);
+	//gameObject.transform.rotation = Quaternion.Slerp(gameObject.transform.rotation, Quaternion.Euler(gameObject.transform.rotation.x, gameObject.transform.rotation.y + rotateBy, gameObject.transform.rotation.z), Time.deltaTime * rotateBy);
+	gameObject.transform.Rotate(0,rotateBy,0);
 	sight.transform.localRotation = Quaternion.Slerp(sight.transform.localRotation, sightTo, Time.deltaTime);
 	// Debug.DrawRay (transform.position, transform.rotation.eulerAngles, Color.blue);
 	Debug.DrawRay(transform.position, transform.forward * 100, Color.green);
@@ -21,6 +23,9 @@ function Update () {
 
 // Update is called once per frame
 public function UpdatePlayerPosition (directionVector:Vector3) :void {
+
+	// Get the input vector from kayboard or analog stick
+	//directionVector = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
 
 	if (directionVector != Vector3.zero) {
 		// Get the length of the directon vector and then normalize it
@@ -40,7 +45,10 @@ public function UpdatePlayerPosition (directionVector:Vector3) :void {
 	}
 	
 	// Apply the direction to the CharacterMotor
-	motor.inputMoveDirection = transform.rotation * directionVector;
+	
+	motor.Move(directionVector);
+
+	//motor.inputMoveDirection = transform.rotation * directionVector;
 
 }
 

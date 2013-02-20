b070bb5414debc940de1645fe9496cfbcb1b0bdb
bloomingbridges <brueckner.florian@googlemail.com>
2013-02-20 02:35:05 +0000

    Created basic distance-based Activatable behaviours
    (C#)

---
+++ unity/Assets/Scripts/Activatable.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+using System.Collections;
+
+public class Activatable : MonoBehaviour {
+
+	public string identifier;
+	public float minDistance = 10.0f;
+	private bool activated;
+	public GameObject controller;
+	private Transform playerRef;
+
+	// Use this for initialization
+	void Start () {
+		playerRef = GameObject.Find("Player").transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (!activated) {
+			float dist = Vector3.Distance(transform.position, playerRef.position);
+			// Debug.Log("Distance to " + identifier + ": " + dist);
+			// Add a delay after which an item gets fully activated
+			// or remove locking for re-activatability :3
+			if (dist <= minDistance) {
+				if (controller) {
+					controller.SendMessage("Activated", identifier);
+					activated = true;
+				}
+			}
+		}
+	}
+}
